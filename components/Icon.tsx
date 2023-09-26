86818fff169a053cfd5f928be92f6183d4f97130
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-09-26 23:15:23 +0400

    adding workout card

---
+++ components/Icon.tsx
@@ -0,0 +1,19 @@
+import { icons } from "@/node_modules/lucide-react";
+
+export type IconName = keyof typeof icons;
+
+export type IconProps = {
+  name: IconName;
+  color?: string | undefined;
+  size?: string | number | undefined;
+  strokeWidth?: string | number | undefined;
+  className?: HTMLElement['className']
+};
+
+const Icon: React.FC<IconProps> = ({ name, color, size, strokeWidth, className }) => {
+  const LucideIcon = icons[name];
+
+  return <LucideIcon strokeWidth={strokeWidth} color={color} size={size} className={className} />;
+};
+
+export default Icon;
