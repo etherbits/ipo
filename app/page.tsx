bcfc26f0aa94d9ccf1f7ec4cff5c4d4fbc19da0b
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-09-30 21:21:21 +0400

    adding workout add button

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,22 @@
+import { Button } from "@/components/ui/button";
+import Icon from "@/components/ui/icon";
 import WorkoutCard from "@/components/ui/workoutCard";
 
 export default function Home() {
   return (
     <main className="">
-      <h1 className="text-2xl font-bold text-center mt-4 mb-6">Workouts</h1>
+      <section className="grid grid-cols-3 items-center mt-4 mb-6">
+        <div />
+        <h1 className="text-2xl font-bold text-center">Workouts</h1>
+        <Button
+          size="icon"
+          className="ml-auto bg-transparent border-none"
+          variant="outline"
+        >
+          <Icon name="Plus" />
+        </Button>
+      </section>
       <ul className="flex flex-col gap-5">
-        <WorkoutCard />
-        <WorkoutCard />
-        <WorkoutCard />
-        <WorkoutCard />
-        <WorkoutCard />
-        <WorkoutCard />
-        <WorkoutCard />
-        <WorkoutCard />
-        <WorkoutCard />
-        <WorkoutCard />
-        <WorkoutCard />
         <WorkoutCard />
       </ul>
     </main>
